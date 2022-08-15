996e6231c6e8fd3ce7081b9b557c93135bac8c53
S David Kong <SDAVIDKONG@gmail.com>
2022-08-15 15:37:03 -0700

    repo init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ethLogo.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          gm, coming soon...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          davidkong.dev
+        </p>
+       
+       
+       
       </header>
     </div>
   );
