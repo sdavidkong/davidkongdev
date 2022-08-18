b9e7ee664df2393ff968e9f42d63f8efbab7b8fe
S David Kong <SDAVIDKONG@gmail.com>
2022-08-17 18:17:00 -0700

    navbar opacity

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,16 +5,19 @@ import {FaUserAstronaut} from 'react-icons/fa'
 import {MdOutlineWork} from 'react-icons/md'
 import {FaHandshake} from 'react-icons/fa'
 import {GrMail} from 'react-icons/gr'
+import { useState } from 'react'
 
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'><ImHome/></a>
-      <a href='#about'><FaUserAstronaut/></a>
-      <a href='#experience'><MdOutlineWork/></a>
-      <a href='#services'><FaHandshake/></a>
-      <a href='#contact'><GrMail/></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><ImHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAstronaut/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdOutlineWork/></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FaHandshake/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><GrMail/></a>
+    
 
     </nav>
   )
